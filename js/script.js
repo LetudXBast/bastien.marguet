23e6d8d75a35cc16a84387dd8767c391d284325c
MARGUET BASTIEN <bastien.marguet@univ-lyon1.fr>
2025-07-01 17:53:30 +0200

    logo

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
   
     /** Smooth scroll pour les ancres */
     function initSmoothScroll() {
-      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      document.querySelectorAll('a[href="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
           e.preventDefault();
           const target = document.querySelector(this.getAttribute('href'));
